a1a073d3144d5a372032cef3e9c07c21132d9fe2
Henrique Soares <henriquesoares.psh@gmail.com>
2023-01-07 17:32:27 -0300

    início seção 7

---
+++ 7 - Tópicos especiais em C#/Seção 7/Program.cs
@@ -0,0 +1,40 @@
+﻿// Aula 86 - Estrutura switch case
+
+//int x = int.Parse(Console.ReadLine());
+//string day;
+//switch (x)
+//{
+//    case 1:
+//        day = "Sunday";
+//        break;
+//    case 2:
+//        day = "Monday";
+//        break;
+//    case 3:
+//        day = "Tuesday";
+//        break;
+//    case 4:
+//        day = "Wednesday";
+//        break;
+//    case 5:
+//        day = "Thursday";
+//        break;
+//    case 6:
+//        day = "Friday";
+//        break;
+//    case 7:
+//        day = "Saturday";
+//        break;
+//    default:
+//        day = "Invalid value";
+//        break;
+//}
+//Console.WriteLine("Day: " + day);
+
+
+
+// Aula 87 Expressão condicional Ternária
+// * Forma resumida do if / else
+
+//double preco = 34.5;
+//double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
