128bb6369282a9e0b68ba9e63af28dc5debf3091
henriquesph <henriquesoares.psh@gmail.com>
2023-03-16 19:17:01 -0300

    herença
    conceitos de herança - protected

---
+++ 10 - Herança e Polimorfismo/seção10/Entities/Account.cs
@@ -0,0 +1,30 @@
+﻿namespace seção10.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; } // private: encapsulamento (pode ser acessado, mas não modificado por outras classes)
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; } // mesma coisa que o private, com a diferença que pode ser alterado por sua subclasse, além da classe principal
+
+        public Account()
+        {
+
+        }
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount) // saque
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ 10 - Herança e Polimorfismo/seção10/Entities/BusinessAcount.cs
@@ -0,0 +1,24 @@
+﻿namespace seção10.Entities
+{
+    class BusinessAcount : Account // Herda ou extende o tipo Account
+    {
+        public double LoanLimit { get; set; } // limite para empréstimo
+
+        public BusinessAcount()
+        {
+        }
+
+        public BusinessAcount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance) // aproveita o construtor da classe base
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan (double amount) // empréstimo
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount; // Balance - atributo da classe base
+            }
+        }
+    }
+}
