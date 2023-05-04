d73f3b11bec5c7ca63f87cc49aa45f70c830d67e
henriquesph <henriquesoares.psh@gmail.com>
2023-05-04 19:58:35 -0300

    falta programa principal

---
+++ 14 - Interfaces/ex208/Ex208/Entities/Installment.cs
@@ -0,0 +1,15 @@
+﻿namespace Ex208.Entities
+{
+    class Installment
+    {
+        public DateTime DueDate { get; set; } // data de vencimento
+        public double Amount { get; set; } // quantia
+
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+    }
+}
---
+++ 14 - Interfaces/ex208/Ex208/Services/ContractService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ex208.Entities;
+
+namespace Ex208.Services
+{
+    class ContractService
+    {
+        
+    }
+}
