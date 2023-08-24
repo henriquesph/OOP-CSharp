826b44e09d754a0fc34e701aa241b5e8c4a8682f
Henrique Soares <62728244+henriquesph@users.noreply.github.com>
2023-08-24 16:21:05 -0300

    exercício

---
+++ 15 - Generics, Set e Dictionary/222 - Dictionary and SortedDictionary/Dictionary/Program.cs
@@ -0,0 +1,81 @@
+﻿//Dictionary - MAP (outras linguagens) < TKey, TValue >
+
+//• É uma coleção de pares chave / valor • Não admite repetições do objeto chave (Estrutura baseada em Hash)
+//• Os elementos são indexados pelo objeto chave (não possuem posição) • Acesso, inserção e remoção de elementos são rápidos
+//• Uso comum: cookies, local storage, qualquer modelo chave-valor
+
+
+//• Dictionary
+//• https://msdn.microsoft.com/en-us/library/xfhwa508(v=vs.110).aspx
+
+
+//• SortedDictionary
+//• https://msdn.microsoft.com/en-us/library/f7fta44c(v=vs.110).aspx
+
+
+// ---------------x---------------------
+
+//Diferenças
+//• Dictionary
+//• Armazenamento em tabela hash (mesmas propriedades do hashset)
+//• Extremamente rápido: inserção, remoção e busca O(1)
+//• A ordem dos elementos não é garantida
+
+//• SortedDictionary (mesmas propriedades do sortedSet)
+//• Armazenamento em árvore
+//• Rápido: inserção, remoção e busca O(log(n)) • Os elementos são armazenados ordenadamente conforme implementação IComparer<T>
+
+
+// ---------------------x--------------
+
+//Alguns métodos importantes
+
+//• dictionary[key] -acessa o elemento pela chave informada
+//• Add(key, value) -adiciona elemento(exceção em caso de repetição) 
+//• Clear() - esvazia a coleção
+//• Count - quantidade de elementos
+//• ContainsKey(key) -verifica se a dada chave existe
+//• ContainsValue(value) -verifica se o dado valor existe
+//• Remove(key) -remove um elemento pela chave
+
+using System.Collections.Generic;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "93949949";
+            cookies["phone"] = "93494484"; // não aceita repetições, valor é sobrescrito.
+
+            Console.WriteLine(cookies["phone"]);
+
+            cookies.Remove("email");
+
+            if(cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("There is no 'email' key");
+            }
+
+
+            Console.WriteLine("Size: " + cookies.Count);
+
+            Console.WriteLine("ALL COOKIES");
+            //foreach(KeyValuePair<string, string> item in cookies)
+            // OU NA FORMA RESUMIDA
+            foreach(var item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
---
+++ 15 - Generics, Set e Dictionary/223 - Ex Dictionary/223 - Ex Dictionary/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                    while(!sr.EndOfStream)
+                    {
+                        string[] votinRecord = sr.ReadLine().Split(',');
+                        string candidate = votinRecord[0];
+                        int votes = int.Parse(votinRecord[1]);
+
+                        if(dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                            //dictionary[candidate] = votes; também poderia ser usado, caso não tenha ele cria par chave/valor, diferente do de cima que daria erro ao adicionar uma chave já existente
+                        }
+                    }
+
+                    foreach(var item in dictionary)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
