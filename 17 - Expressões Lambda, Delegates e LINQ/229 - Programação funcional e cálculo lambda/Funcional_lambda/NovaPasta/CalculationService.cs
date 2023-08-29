5dcef8b2b570f65960ac18e1da2c6cb6329960d5
Henrique Soares <62728244+henriquesph@users.noreply.github.com>
2023-08-29 18:12:22 -0300

    funcional - lambda

---
+++ 17 - Expressões Lambda, Delegates e LINQ/229 - Programação funcional e cálculo lambda/Funcional_lambda/NovaPasta/CalculationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// static: método de classe, não precisa instanciar objeto para ser executado, não depende do estado do objeto, ou seja, dos valores do objeto instanciado, os valores são passados na classe que executa, exemplo: MATH
+// todos os objetos criado apontam para o mesmo endereço de memória static
+
+namespace Course.Services
+{
+    class CalculationService
+    {
+        public static double Max(double x, double y)
+        {
+            return (x > y) ? x : y;
+        }
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Square(double x)
+        {
+            return x * x;
+        }
+    }
+}
---
+++ 17 - Expressões Lambda, Delegates e LINQ/229 - Programação funcional e cálculo lambda/Funcional_lambda/Program.cs
@@ -0,0 +1,83 @@
+﻿// Ver mais detalhes na apostila
+
+// Paradigma funcional de programação
+// Baseado no formalismo matemático Cálculo Lambda (Church 1930)
+
+
+// Transparência referencial:
+// Uma função possui transparência referencial se seu resultado for sempre o mesmo para os mesmos dados de entrada. Benefícios: simplicidade e previsibilidade.
+
+// Funções são objetos de primeira ordem (ou primeira classe)
+// Isso significa que funções podem, por exemplo, serem passadas como parâmetros de métodos, bem como retornadas como resultado de métodos
+
+// Inferência de tipos
+
+
+
+// Expressividade / "como" vs. "o quê"
+
+
+//int sum = 0;
+//foreach (int x in list)
+//{
+//    sum += x;
+//}
+//vs.
+//int sum = list.Aggregate(0, (x, y) => x + y);
+
+
+
+//O que são "expressões lambda"?
+//Em programação funcional, expressão lambda corresponde a uma
+//função anônima de primeira classe.
+
+
+// Cálculo Lambda = formalismo matemático base da programação funcional
+
+// Expressão lambda = função anônima de primeira classe
+
+
+// --------------------X-----------------------
+
+// AULA 230: Introdução a Delegates
+
+//• É uma referência (com type safety) para um ou mais métodos
+//• É um TIPO referência, diferente de struct que é um tipo valor
+
+//• Usos comuns:
+//• Comunicação entre objetos de forma flexível e extensível (eventos / callbacks)
+//• Parametrização de operações por métodos (programação funcional)
+
+
+
+//Delegates pré-definidos
+//• Action
+//• Func
+//• Predicate
+
+using Course.Services;
+
+namespace Course
+{
+    delegate double BinaryNumericOperation(double n1, double n2); // type safety: só funciona passando os mesmos parâmetros (assinatura da função), não funciona com a função square que só passa 1 double, pois o delegate neste caso pede 2
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            // BinaryNumericOperation op = CalculationService.Max;
+
+            // SINTAXE ALTERNATIVA - mais "verbosa":
+            BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+
+            //double result = op(a, b);
+            // SINTAXE ALTERANATIVA - Invoke
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+
+        }
+    }
+}
