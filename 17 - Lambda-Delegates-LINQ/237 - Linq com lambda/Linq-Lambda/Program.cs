b8cd8a38b5c3dcf4a8302b21ddda4f266f643014
Henrique Soares <62728244+henriquesph@users.noreply.github.com>
2023-09-05 16:24:48 -0300

    update

--- 17 - Lambda-Delegates-LINQ/237 - Linq com lambda/Linq-Lambda/Program.cs
+++ 17 - Lambda-Delegates-LINQ/237 - Linq com lambda/Linq-Lambda/Program.cs
@@ -49,6 +49,38 @@ namespace Course
             Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT", r3);
 
             var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name); // ordenando por 2 predicados
+
+            var r5 = r4.Skip(2).Take(4); // pula 2 elementos e pega 4 elementos
+
+            //var r6 = products.First();
+            var r6 = products.FirstOrDefault(); // se encontrar um lista vazia retorna Null, diferente do First que dá erro.
+            Console.WriteLine("First test1: " + r6);
+
+            var r8 = products.Where(p => p.Id == 3).SingleOrDefault(); // pega 1 elemento com esse predicado do tipo Product, se nao tivesse SingleOrDefault retornaria objeto do tipo IEnumerable<Product>
+            // Caso retorne mais de um elemento ocorre uma exceção
+            // caso não retorne nenhum valor retorna nulo, diferente do sigle que dá exceção
+            Console.WriteLine("Single or Default test1: " + r8);
+
+
+            // 238 - PARTE 2
+            // OPERAÇÕES DE AGREGAÇÃO
+
+            var r10 = products.Max(p => p.Price); // o Predicado implementa o IComparable
+            Console.WriteLine("Max Price: " + r10);
+
+            var r11 = products.Min(p => p.Price);
+            Console.WriteLine("Min Price: " + r11);
+
+            var r12 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
+            Console.WriteLine("Category 1 sum prices: " + r12);
+
+            var r13 = products.Where(p => p.Category.Id == 1).Average(p => p.Price);
+            Console.WriteLine("Category 1 Average prices: " + r13); // se não tiver elementos dá erro (divisão por zero)
+
+            // Corrigindo o erro acima
+            var r14 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty(0.0).Average(); // se estiver vazio passa o zero
+            Console.WriteLine("Category 5 Average prices: " + r14);
         }   
     }
 }
+5:30
